e5e8056ea151729e1582afbfc52a83f23250f59e
Marr7781 <134134957+Marr7781@users.noreply.github.com>
2025-03-14 21:31:06 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // Import path module
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: path.resolve(__dirname, './client'), // Tambahkan opsi root
+});
