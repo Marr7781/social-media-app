18f15efe94ce08ddb8d55030beade4ee6ca57169
Marvel <marveltjenyani8@gmail.com>
2025-03-16 17:03:12 +0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -36,8 +36,7 @@ const cors = require('cors');
 //! DON'T FORGET TO CHANGE THE ORIGIN
 app.use(cors({
     origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 //servertest
