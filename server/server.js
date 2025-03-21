d82fdec371ec9b5e039a42d6155bfc32bc4f3719
Marvel <marveltjenyani8@gmail.com>
2025-03-21 15:18:16 +0700

    fixing bug

--- server/server.js
+++ server/server.js
@@ -87,7 +87,7 @@ app.post(`/login`, (req, res)=> {
 
                 res.cookie('token', token, {
                     httpOnly: true,
-                    secure: true,
+                    secure: process.env.NODE_ENV === 'production',
                     sameSite: 'strict',
                     maxAge: 3600000,
                 });
